aae139183ed415e75fcad7d69712c127c8fca538
Uday Jamgade <udayjamgade1@gmail.com>
2025-09-22 00:05:05 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
-  base: process.env.VITE_BASE_URL || '/MangaStore'
+  // base: process.env.VITE_BASE_URL || '/MangaStore'
 })
 
